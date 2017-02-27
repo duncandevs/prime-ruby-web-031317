a017d640964679c1814774ce7bc1e29ebeb8c5cd
Duncan Maina <duncandevs@gmail.com>
2017-02-27 17:31:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  int = integer.abs
+  if int == 1 || int == 0
+    return false
+  end
+  (2...int).each do |number|
+    if int % number == 0
+      return false
+    end
+  end
+  return true
+end
